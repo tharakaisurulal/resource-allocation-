94dbc91a3e94aa4dc8955e53f660acef1d18ce69
FERNANDO C.I.M <fernando10881@usci.ruh.ac.lk>
2023-09-11 21:45:58 +0530

    /home h2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -72,6 +72,7 @@ if (isset($_SESSION['User_Fname'])){
 <section class="services">
 
    <h1 class="heading-title"> Today Allocation </h1>
+   <h2>cs department</h2>
 
    <div class="box-container">
 
