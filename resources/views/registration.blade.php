4754dc13dd9b1cb367b2733e3663c33f2b527b1e
FERNANDO C.I.M <fernando10881@usci.ruh.ac.lk>
2023-09-12 06:20:51 +0530

    Remove homepages and recode forms

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -24,11 +24,6 @@
                         <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
                         
                     </div>
-                       <br> 
-                    <div class="form-grouprow">
-                        <label for="inputdepartment3" class="col-sm-2 col-form-label">Department :</label>
-                                <input type="department" class="form-control" id="inputdepartment3" placeholder="department">       
-                    </div>
                     <br>
                     <div class="form-grouprow">
                     <label for="inputfaculty3" class="col-sm-2 col-form-label">Faculty  : </label>
@@ -42,6 +37,11 @@
                             <option value="op7">Faculty of Technology</option>
                             <option value="op7">Faculty of Agriculture</option>
                             </select>
+                    </div>
+                    <br> 
+                    <div class="form-grouprow">
+                        <label for="inputdepartment3" class="col-sm-2 col-form-label">Department :</label> 
+                        <input type="text" class="form-control" id="inputDepartment3" placeholder="Department of Computer Science">    
                     </div>
                     <br>
                     <div class="form-grouprow">
