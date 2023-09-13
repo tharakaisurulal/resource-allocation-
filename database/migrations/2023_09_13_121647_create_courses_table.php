4202f346cc70dc798d0c113190cab95f7aa46ec5
tharakaisurulal <96917598+tharakaisurulal@users.noreply.github.com>
2023-09-13 18:15:48 +0530

    added tables

--- database/migrations/2023_09_13_121647_create_courses_table.php
+++ database/migrations/2023_09_13_121647_create_courses_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->String ('course_name');
+            $table->String ('course_code');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courses');
     }
 };
--- database/migrations/2023_09_13_121920_create_labs_table.php
+++ database/migrations/2023_09_13_121920_create_labs_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('labs', function (Blueprint $table) {
+            $table->id();
+            $table->String ('lab_name');
+            $table->String ('lab_capacity')->nullable();
+            $table->String ('lab_air_conditioner')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('labs');
     }
 };
