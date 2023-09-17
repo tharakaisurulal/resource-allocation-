10678d4e371aeeb5f1843ee3d8c98b703d0f23b8
FERNANDO C.I.M <fernando10881@usci.ruh.ac.lk>
2023-09-17 22:21:48 +0530

    guest request form

--- resources/views/requestform.blade.php
+++ resources/views/requestform.blade.php
@@ -1,39 +1,55 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/regi.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('css/request.css')}}">
     </head>
     <body>
+<<<<<<< Updated upstream
         <div class ="container">
                 
                 <form class="formcl">
                 <h1>SEND YOUR REQUEST</h1>
                 <br>
                     <div class="form-grouprow">
+=======
+            <div class ="login">
+                <div class="box">
+                    <form>
+                        <h1>Send Your Request</h1>
+                            <br>
+                        <div class="inputBox">
+>>>>>>> Stashed changes
                             <label for="inputname3" class="col-sm-2 col-form-label">Initial Name:</label>
                             <input type="text" class="form-control" id="inputname3" placeholder="G.R.Perera">
-                    </div>
-                    <br>
-                    <div class="form-grouprow">
-                        <label for="inputEmail3" class="col-sm-2 col-form-label">Email:</label>
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="email@gmail.com">
-                    </div>
-                    <br>
-                    <div class="form-grouprow">
+                            <i><ion-icon name="lock-closed"><icon-icon></i>
+                        </div>
+                        <br>
+                        <div class="inputBox">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Email:</label>
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="email@gmail.com">
+                            <i><ion-icon name="lock-closed"><icon-icon></i>
+                        </div>
+                        <br>
+                        <div class="inputBox">
                             <label for="inputdate3" class="col-sm-2 col-form-label">Date:</label>
                             <input type="Date" class="form-control" id="inputdate3">
-                    </div>
-                    <br>
-                    <div class="form-grouprow">
+                            <i><ion-icon name="lock-closed"><icon-icon></i>
+                        </div>
+                        <br>
+                        <div class="inputBox">
                             <label for="input_time3" class="col-sm-2 col-form-label">Time:  from </label>
                             <input type="time" class="form-control" id="input_time3">
-
+                            <i><ion-icon name="lock-closed"><icon-icon></i>
+                        </div>
+                        <div class="inputBox">
                             <label for="input_time3" class="col-sm-2 col-form-label">To </label>
                             <input type="time" class="form-control" id="input_time3">
-                    </div>
-                    <br>
-                    <div class="form-grouprow">
-                    <label for="inputlocation3" class="col-sm-2 col-form-label">Location:</label>
+                            <i><ion-icon name="lock-closed"><icon-icon></i>
+                        </div>
+                        <br>
+                        <div class="inputBox">
+                            <label for="inputlocation3" class="col-sm-2 col-form-label">Location:</label>
                             <select name="location" id="location">
                             <option value="op1">lab 1</option>
                             <option value="op2">lab 2</option>
@@ -43,10 +59,10 @@
                             <option value="op6">lecture hall 03</option>
                             <option value="op7">Main auditorium</option>
                             </select>
-                    </div>
-                    <br>
-                    <div class="form-grouprow">
-                    <label for="inputfaculty3" class="col-sm-2 col-form-label">Faculty  : </label>
+                        </div>
+                        <br>
+                        <div class="inputBox">
+                            <label for="inputfaculty3" class="col-sm-2 col-form-label">Faculty  : </label>
                             <select name="faculty" id="faculty">
                             <option value="op1">Faculty of Science</option>
                             <option value="op2">Faculty of Management and Finance</option>
@@ -57,19 +73,25 @@
                             <option value="op7">Faculty of Technology</option>
                             <option value="op7">Faculty of Agriculture</option>
                             </select>
-                    </div>
-                    <br>
-                    <div class="form-grouprow">
-                        <label for="inputdepartment3" class="col-sm-2 col-form-label">Department :</label> 
-                        <input type="text" class="form-control" id="inputDepartment3" placeholder="Department of Computer Science">    
-                    </div>
-                    <br>
-                    <div class="form-grouprow">
-                        <div class="col-sm-10">
-                        <button type="reset" class="btn btn-primary">Reset</button>
-                        <button type="register" class="btn btn-primary">Submit</button>
                         </div>
+<<<<<<< Updated upstream
                 </div>
         </div>
+=======
+                        <br>
+                        <div class="inputBox">
+                            <label for="inputdepartment3" class="col-sm-2 col-form-label">Department :</label> 
+                            <input type="text" class="form-control" id="inputDepartment3" placeholder="Department of Computer Science">
+                            <i><ion-icon name="lock-closed"><icon-icon></i>    
+                        </div>
+                        <br>
+                        <div class="inputBox">
+                            <div class="col-sm-10">
+                            <button type="reset" class="btn btn-primary">Reset</button>
+                            <button type="register" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+            </div>
+>>>>>>> Stashed changes
     </body>
 </html>
