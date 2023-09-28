2a3b64532e4a684cdd478a745028a4a1754f2a58
tharakaisurulal <96917598+tharakaisurulal@users.noreply.github.com>
2023-09-28 13:35:16 +0530

    changes

---
+++ app/Http/Controllers/lecturerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\lecturer;
+use App\Models\lecturerrequest;
+use Illuminate\View\View;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\hash;
+use session;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Contracts\Hashing\Hasher;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\DB;
+//use Cookie;
+use Illuminate\Http\Response;
+//use Symfony\Component\HttpFoundation\Cookie;
+use Illuminate\Support\Facades\Cookie;
+
+
+class lecturerController extends Controller
+{
+    //
+}
