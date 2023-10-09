255e9993d779c98a0bfd036e734345bedd7aaef8
Champika98 <142742002+Champika98@users.noreply.github.com>
2023-10-09 14:26:28 +0530

    add to allocate lab
    style sheet and allocate page

---
+++ resources/views/allocateLab.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href={{asset('css/style1.css')}}>
+    <link rel="stylesheet" href={{asset('css/allocateLab.css')}}>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+    <link rel="stylesheet" href={{asset('css/bootstrap.css')}}>
+
+</head>
+<body>
+    <div class="card">
+        <div class="col-6">
+            <img src="images/p.jfif" alt="">
+        </div>
+        <div class="card-body">
+            <h1 class="card-title"> Laboratory 01</h1>
+            <p class="card-info">
+                    Capacity :     <br>
+                    Ac or Non Ac : <br>
+                    Location
+            </p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="col-6">
+            <img src="images/Computer1.jfif" alt="">
+        </div>
+        <div class="card-body">
+            <h1 class="card-title"> Laboratory 02</h1>
+            <p class="card-info">
+                    Capacity :     <br>
+                    Ac or Non Ac : <br>
+                    Location
+            </p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="col-6">
+            <img src="images/20160122_1327231.jpg" alt="">
+        </div>
+        <div class="card-body">
+            <h1 class="card-title">Reserach Lab</h1>
+            <p class="card-info">
+                    Capacity :     <br>
+                    Ac or Non Ac : <br>
+                    Location
+            </p>
+        </div>
+    </div>
+</body>
+</html>
