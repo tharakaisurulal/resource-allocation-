7262a5b9c0f8c0a796b4548512ac1d6987f980e7
Champika98 <142742002+Champika98@users.noreply.github.com>
2023-10-18 09:08:14 +0530

    add to about page
    about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,54 @@
+<html>
+ <head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    @include('cdn')
+	<title>ABOUT</title>
+
+    <link rel="stylesheet" href={{asset('css/about.css')}}>
+
+    <!---boxicons links-->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!--remixicons link-->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+
+    <!-- scrollreveal effect-->
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+    <!--custom js link --->
+
+
+ </head>
+
+
+<body>
+
+    <header>
+
+        <img src="images/logo_copy.png" class="logo">
+
+        <div class="navlist">
+            <a href="#">Home</a>
+            <a href="#">Staff</a>
+            <a href="#">Programs</a>
+            <a href="#">Research</a>
+            <a href="#">Services</a>
+            <a href="#">contact</a>
+            <a href="#">Log Out</a>
+        </div>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+    </header>
+
+
+
+
+
+
+</body>
+
+</html>
