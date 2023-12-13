431696f268f11e1c0c045d21acef33c69675b3c8
Champika98 <142742002+Champika98@users.noreply.github.com>
2023-12-14 01:39:04 +0530

    change filter time table

--- resources/views/academicsupportive/filtertimetable.blade.php
+++ resources/views/academicsupportive/filtertimetable.blade.php
@@ -11,9 +11,14 @@
 
     </head>
     <body class="bdy">
+
+      <div class="box">
         <div class="div1">
+            <form action="{{route('filtertimetablesubmit')}}" method="post">
+            @csrf
+
             <div class="text-center">
-                <h1>Timetable Filter</h1>
+                <h1><b>Timetable Filter</b></h1>
             </div>
 
             @if(session()->has('success'))
@@ -24,10 +29,7 @@
             <div class="alert alert-danger">{{session()->get('fail')}}</div>
         @endif
 
-        <div class="con1">
-    <form action="{{route('filtertimetablesubmit')}}" method="post">
-            @csrf
-             <label for="name"><b>Program:</b></label>
+            <label for="name"><b>Program:</b></label>
              <select name="prog" id="faculty">
                  <option value="">--Choose Program--</option>
                  @foreach ($dater2 as $cdb )
@@ -84,13 +86,15 @@
                                 Submit
                             </button><br>
                             </div><br>
-    </form>
-     </div>
+            </form>
+        </div>
+</div>
 
-     <h4>Theory</h4>
+<div class="box1">
+     <h4><b>Theory</b></h4>
          <div class="d-flex justify-content-center div2">
             @if (isset($joindata))
-        <table class="table table-dark">
+         <table class="table table-dark">
             <thead>
                 <td>Start Time</td>
                 <td>End Time</td>
@@ -116,15 +120,15 @@
 
 
 
-   <div>
+
 
     @else
     <div class="alert alert-danger">{{ 'Enter valid details' }}</div>
     @endif
     </div><br>
-</div>
 
-<h4>Practical </h4>
+
+<h4><b>Practical</b></h4>
 <div class="d-flex justify-content-center div2">
     @if (isset($joindata1))
     <table class="table table-dark">
@@ -152,17 +156,16 @@
   </table>
 
 <div>
-
+</div>
 @else
 <div class="alert alert-danger">{{ 'Enter valid details' }}</div>
 @endif
 </div>
 
-         </div>
+</div>
 
-        </div>
-
-    </body>
+</div>
+</body>
 </html>
 
 
