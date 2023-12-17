d5dd5c6ef42d35241b79f71f1d769259313d4349
dasuni <dasuninuwanthika344@gmail.com>
2023-12-17 10:08:57 +0530

    update  viewprofile
    update  viewprofile

--- resources/views/students/viewProfilestu.blade.php
+++ resources/views/students/viewProfilestu.blade.php
@@ -40,18 +40,15 @@
 
 
         <div class="links">
-            <a class="ResearchGate" href=
-"https://www.ResearchGate.com/geeksforgeeks.org/"
+            <a class="ResearchGate" href="https://www.ResearchGate.com/geeksforgeeks.org/"
                 target="_blank" title="GFG_ResearchGate">
                 <i class="fab fa-ResearchGate"></i>
             </a>
-            <a class="git" href=
-"https://github.com/topics/geeksforgeeks"
+            <a class="git" href="https://github.com/topics/geeksforgeeks"
                 title="GFG_github" target="_blank">
                 <i class="fab fa-github-square"></i>
             </a>
-            <a class="linkedin" href=
-"https://www.geeksforgeeks.org/tag/linkedin/"
+            <a class="linkedin" href="https://www.geeksforgeeks.org/tag/linkedin/"
                 title="GFG_linkedin" target="_blank">
                 <i class="fab fa-linkedin"></i>
             </a>
