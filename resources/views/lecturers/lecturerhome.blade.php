37122d76815df470e3c90d5c0064a68624bb7fd9
Champika98 <142742002+Champika98@users.noreply.github.com>
2023-12-17 10:59:07 +0530

    change lecture home page

--- resources/views/lecturers/lecturerhome.blade.php
+++ resources/views/lecturers/lecturerhome.blade.php
@@ -53,12 +53,12 @@
 
             <div class="face front">
                 <img src="/images/lab1.jpeg" alt="" width=300px height=350px>
-                <h3> Labortory</h3>
+                <h3> Laboratory</h3>
             </div>
 
             <div class="face back">
                     <div class="back-content center">
-                        <h3> Labortory</h3>
+                        <h3> Laboratory</h3>
                         <div class="button">
                             <a href="/lecturers/lab"><span>CLICK</span></a>
                         </div>
